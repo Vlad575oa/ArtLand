d4fcca79e61288702fce023862ea2b7312c1d8d9
Vlad <vlad575@mail.ru>
2026-02-18 15:02:18 +0300

    chore: optimize font loading with preconnect

--- components/layout/NonBlockingIcons.tsx
+++ components/layout/NonBlockingIcons.tsx
@@ -4,17 +4,20 @@ import Script from 'next/script'
 
 export function NonBlockingIcons() {
     return (
-        <Script
-            id="material-icons"
-            strategy="lazyOnload"
-            dangerouslySetInnerHTML={{
-                __html: `
+        <>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+            <Script
+                id="material-icons"
+                strategy="lazyOnload"
+                dangerouslySetInnerHTML={{
+                    __html: `
                     var link = document.createElement('link');
                     link.rel = 'stylesheet';
                     link.href = 'https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Round&display=swap';
                     document.head.appendChild(link);
                 `
-            }}
-        />
+                }}
+            />
+        </>
     )
 }
